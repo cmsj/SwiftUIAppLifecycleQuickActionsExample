2bb1c4b1b290c31c08c2622225f200b8d29b5a5b
Chris Jones <cmsj@tenshu.net>
2020-08-06 11:30:31 +0100

    Initial Commit

---
+++ Shortcats/ShortcatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortcatsApp.swift
+//  Shortcats
+//
+//  Created by Chris Jones on 06/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct ShortcatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
