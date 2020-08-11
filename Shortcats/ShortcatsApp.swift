d21c8d110ffadf101ca04da676af3e259def7637
Chris Jones <cmsj@tenshu.net>
2020-08-11 23:05:16 +0100

    Fixes from Apple

--- Shortcats/ShortcatsApp.swift
+++ Shortcats/ShortcatsApp.swift
@@ -26,6 +26,15 @@ class AppDelegate: NSObject, UIApplicationDelegate, ObservableObject {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         let viewModel = ViewModel.shared
 
+        // Set a Quick Action
+        let icon = UIApplicationShortcutIcon(systemImageName: "bell")
+        let item = UIApplicationShortcutItem(type: "meow",
+                                             localizedTitle: "Meow",
+                                             localizedSubtitle: "Make the app more catty",
+                                             icon: icon,
+                                             userInfo: nil)
+        UIApplication.shared.shortcutItems = [item]
+
         viewModel.didFinishCalled = "checkmark.circle"
 
         if let shortcutItem = launchOptions?[UIApplication.LaunchOptionsKey.shortcutItem] as? UIApplicationShortcutItem {
@@ -44,22 +53,33 @@ class AppDelegate: NSObject, UIApplicationDelegate, ObservableObject {
         viewModel.didPerformAF = "checkmark.circle"
         viewModel.shortcutType = shortcutItem.type
     }
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let config = UISceneConfiguration()
+        config.delegateClass = MySceneDelegate.self
+        return config
+    }
+}
+
+class MySceneDelegate : NSObject, UIWindowSceneDelegate {
+    func windowScene(
+        _ windowScene: UIWindowScene,
+        performActionFor shortcutItem: UIApplicationShortcutItem,
+        completionHandler: @escaping (Bool) -> Void
+    ) {
+        let viewModel = ViewModel.shared
+        viewModel.didPerformAF = "checkmark.circle"
+        viewModel.shortcutType = shortcutItem.type
+    }
 }
 
 @main
 struct ShortcatsApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-
-    init() {
-        // Set a Quick Action
-        let icon = UIApplicationShortcutIcon(systemImageName: "bell")
-        let item = UIApplicationShortcutItem(type: "meow",
-                                             localizedTitle: "Meow",
-                                             localizedSubtitle: "Make the app more catty",
-                                             icon: icon,
-                                             userInfo: nil)
-        UIApplication.shared.shortcutItems = [item]
-    }
 
     var body: some Scene {
         WindowGroup {
